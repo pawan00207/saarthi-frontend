28db4b793d8e8706abcdda9fbd99498a10f2a38b
Pawan Singh <pawan9140582015@gmail.com>
2025-07-04 17:17:11 +0530

    Fix: Added missing ToneChecker component

---
+++ components/ToneChecker.js
@@ -0,0 +1,10 @@
+// src/components/ToneChecker.js
+import React from "react";
+
+export default function ToneChecker() {
+  return (
+    <div>
+      <h2>Tone Checker Component Placeholder</h2>
+    </div>
+  );
+}
