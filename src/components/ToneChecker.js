07de44530d268e8f06743488b83b6803a74a41bc
Pawan Singh <pawan9140582015@gmail.com>
2025-07-04 17:39:58 +0530

    Add missing components: ToneChecker and FeedInfo

---
+++ src/components/ToneChecker.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ToneChecker = () => {
+  return (
+    <div className="p-4 bg-blue-100 rounded">
+      <h2 className="text-lg font-bold mb-2">Tone Checker</h2>
+      <p>This is a placeholder for the ToneChecker component.</p>
+    </div>
+  );
+};
+
+export default ToneChecker;
